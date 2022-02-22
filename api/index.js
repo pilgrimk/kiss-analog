84368f07a0c08bbdb9e494a337ac52c7c005bfbf
Kevin Pilgrim <pilgrimka1@yahoo.com>
2022-02-21 18:38:45 -0700

    Added CORS_ORIGIN

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
 app.use('/images', express.static(path.join(__dirname, '/images')));
-app.use(cors());
+app.use(cors({origin : `${process.env.CORS_ORIGIN}`}));
 /*app.use(function (req, res, next) 
 {
       res.header("Access-Control-Allow-Origin", "");
