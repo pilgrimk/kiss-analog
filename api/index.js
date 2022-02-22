4692473a4adea5c318fbd125ade7430d232da2b7
Kevin Pilgrim <pilgrimka1@yahoo.com>
2022-02-21 20:51:43 -0700

    Fixing Static file

--- api/index.js
+++ api/index.js
@@ -62,9 +62,10 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
+app.get('*', function (req, res) {
+const index = path.join(__dirname, 'build', 'index.html');
+res.sendFile(index);
+});  
 
 app.listen(port, () => {
     console.log("Server is listening!")
